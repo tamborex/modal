94ee02d81f5c0b159f61610c0d79f652d01384ab
Tamborex <tamborexj040@gmail.com>
2024-08-26 12:32:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* document.getElementById ("exibir").addEventListener("click", function() {
+    var div = document.getElementById ("pop-up");
+    div.style.display = "block";
+}); */
+
+/* function exibir() {
+    var display = document.getElementById("pop-up").style.display
+    if ( display == 'none')
+        document.getElementById("pop-up").style.display = 'block';
+    else
+        document.getElementById("pop-up").style.display = 'none';
+} */
